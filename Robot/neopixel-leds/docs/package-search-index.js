b1c9e288a6748da997af07187b54c8217476ff9f
Jordan Powers <jordanpowers1227@gmail.com>
2022-03-29 15:32:21 -0700

    Remove old doc location

--- Robot/neopixel-leds/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.usfirst.frc.team4999.lights"},{"l":"org.usfirst.frc.team4999.lights.animations"},{"l":"org.usfirst.frc.team4999.lights.commands"},{"l":"org.usfirst.frc.team4999.lights.compositor"}]
