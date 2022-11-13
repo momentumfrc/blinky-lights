dd6b25e55c1bcdfc168e5afa63cc4dcf1b413583
Jordan Powers <jordanpowers1227@gmail.com>
2022-11-12 16:58:14 -0800

    Add AudioInfoSource.kt

---
+++ Pi/blinky-pi/app/src/main/kotlin/com/momentum4999/lights/pi/audiostream/AudioInfoSource.kt
@@ -0,0 +1,40 @@
+package com.momentum4999.lights.pi.audiostream
+
+import java.lang.ProcessBuilder
+import java.lang.Process
+import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class AudioInfoSource : AutoCloseable {
+    class AudioInfo {
+        var averageAmplitude: Short = 0
+        var isBeat: Boolean = false
+    }
+
+    private val subprocess: Process
+    private val dataStream: InputStream
+
+    private var dataBuff = byteArrayOf(0, 0, 0)
+    private var dataByteBuff = ByteBuffer.wrap(dataBuff).order(ByteOrder.LITTLE_ENDIAN)
+
+    init {
+        val builder = ProcessBuilder("/home/jordan/blinky-lights/Pi/audio-shim/shim")
+        builder.redirectOutput(ProcessBuilder.Redirect.PIPE)
+        builder.redirectError(ProcessBuilder.Redirect.DISCARD)
+        subprocess = builder.start()
+        dataStream = subprocess.getInputStream()
+    }
+
+    override fun close() {
+        subprocess.destroy()
+    }
+
+    fun getInfo(info: AudioInfo): Unit {
+        dataStream.readNBytes(dataBuff, 0, 3)
+
+        dataByteBuff.rewind()
+        info.averageAmplitude = dataByteBuff.getShort()
+        info.isBeat = dataByteBuff.get() != 0.toByte()
+    }
+}
